710e85ad5ecc4f02dc14f1aeb0b65449a8110f24
Peifan Li <frank.li.oxygen@gmail.com>
2025-11-27 20:54:44 -0500

    style: Improve speed calculation and add version in footer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import packageJson from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +8,7 @@ export default defineConfig({
   server: {
     port: 5556,
   },
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version)
+  }
 });
