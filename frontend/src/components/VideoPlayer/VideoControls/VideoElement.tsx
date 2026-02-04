a85a15688d85e4c0668fc9b793310ae94e1a7c7a
Peifan Li <frank.li.oxygen@gmail.com>
2026-02-04 18:31:58 -0500

    Revert "style: Update video element styling for better control"
    This reverts commit c1f72e91ee8b6f324ffcd000b07e34089150fe2c.

--- frontend/src/components/VideoPlayer/VideoControls/VideoElement.tsx
+++ frontend/src/components/VideoPlayer/VideoControls/VideoElement.tsx
@@ -86,14 +86,7 @@ const VideoElement: React.FC<VideoElementProps> = ({
     }, []);
 
     return (
-        <Box
-            sx={{
-                position: 'relative',
-                width: '100%',
-                aspectRatio: '16/9',
-                overflow: 'hidden'
-            }}
-        >
+        <>
             {/* Scoped style for centering subtitles */}
             <style>
                 {`
@@ -163,16 +156,7 @@ const VideoElement: React.FC<VideoElementProps> = ({
             <video
                 id={videoId}
                 ref={videoRef}
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'contain',
-                    display: 'block',
-                    cursor: 'pointer'
-                }}
+                style={{ width: '100%', aspectRatio: '16/9', display: 'block', cursor: 'pointer' }}
                 controls={false}
                 src={src}
                 preload={preloadStrategy}
@@ -225,7 +209,7 @@ const VideoElement: React.FC<VideoElementProps> = ({
                 ))}
                 Your browser does not support the video tag.
             </video>
-        </Box>
+        </>
     );
 };
 
